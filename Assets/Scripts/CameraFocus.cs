30c5193a4b64955c06328f1add964d4753113aef
Gustav <sir.gustav.the.coder@gmail.com>
2015-06-19 08:57:42 +0200

    Added standard camera

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFocus : MonoBehaviour {
+
+	public GameObject Player;
+	
+	private Vector3 Offset;
+	
+	void Start ()
+	{
+		this.Offset = this.transform.position - this.Player.transform.position;
+	}
+	
+	void LateUpdate ()
+	{
+		this.transform.position = this.Player.transform.position + this.Offset;
+	}
+}
