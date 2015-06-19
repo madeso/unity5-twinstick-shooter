cfac7510f882097f6571a3e52243410b32c5f472
Gustav <sir.gustav.the.coder@gmail.com>
2015-06-19 08:43:26 +0200

    added basic player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float Speed;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		this.rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		var h = Input.GetAxis ("Horizontal");
+		var v = Input.GetAxis ("Vertical");
+		var move = new Vector3 (h, 0.0f, v);
+		
+		this.rb.AddForce (move * this.Speed);
+	}
+}
